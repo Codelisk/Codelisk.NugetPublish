ea2165c1a12fb0872b8e67b91e41be9cd43c3fa1
Daniel Hufnagl <d.hufnagl@codelisk.com>
2023-08-03 14:49:25 +0200

    Test

--- build/Tasks/Library/NugetIncrementVersionTask.cs
+++ build/Tasks/Library/NugetIncrementVersionTask.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace Codelisk.NugetPublish.Tasks.Library
 {
@@ -30,32 +32,37 @@ namespace Codelisk.NugetPublish.Tasks.Library
             var propsFilePath = "../../../Directory.Build.props"; // Update with the correct file path
 
             // Increment the version and update the file
-            IncrementAndSaveVersion(propsFilePath);
+            IncrementAndSaveVersion(propsFilePath, context);
         }
         // Increment the version and update the file
-        void IncrementAndSaveVersion(string filePath)
+        void IncrementAndSaveVersion(string filePath, BuildContext context)
         {
             var xmlContent = File.ReadAllText(filePath);
 
             // Find the Version element and increment it
             xmlContent = UpdateVersion(xmlContent);
-
+            context.Log.Warning(xmlContent);
             // Write the updated content back to the file
             File.WriteAllText(filePath, xmlContent);
         }
 
-        // Update the Version element in the XML content
         private string UpdateVersion(string xmlContent)
         {
-            var xml = new System.Xml.XmlDocument();
-            xml.LoadXml(xmlContent);
+            var doc = XDocument.Parse(xmlContent);
 
-            var versionNode = xml.SelectSingleNode("//PropertyGroup[Condition=\"'$(Configuration)' == 'Release'\"]/Version");
-            if (versionNode != null && Version.TryParse(versionNode.InnerText, out var version))
+            var namespaceName = doc.Root.GetDefaultNamespace().NamespaceName;
+            XNamespace ns = namespaceName;
+
+            var versionNode = doc.Descendants("PropertyGroup")
+                                 .Where(pg => pg.Attribute("Condition")?.Value == "'$(Configuration)' == 'Release'")
+                                 .Elements("Version")
+                                 .FirstOrDefault();
+
+            if (versionNode != null && Version.TryParse(versionNode.Value, out var version))
             {
                 var incrementedVersion = new Version(version.Major, version.Minor, version.Build + 1, version.Revision);
-                versionNode.InnerText = incrementedVersion.ToString();
-                return xml.OuterXml;
+                versionNode.Value = incrementedVersion.ToString();
+                return doc.ToString();
             }
 
             throw new InvalidOperationException("Version element not found or invalid in the XML.");
